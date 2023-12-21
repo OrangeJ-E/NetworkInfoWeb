50d19f3844bd073803d7f00e7ed5ecfae976673f
OrangeJ-FFC <OrangeJ-E@outlook.com>
2023-12-21 18:43:20 +0800

    1.1.1

--- src/main/java/org/network/networkinfo/repository/AddressRepository.java
+++ src/main/java/org/network/networkinfo/repository/AddressRepository.java
@@ -2,8 +2,12 @@ package org.network.networkinfo.repository;
 
 import org.network.networkinfo.model.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+@Repository
 public interface AddressRepository extends JpaRepository<Object, Long> {
     // 可以添加自定义的查询方法
+    Optional<Object> findById(Long aLong);
+
     Address findByWlanMacAddress(String wlanMacAddress);
 }
